a1b42ab95f3c19c2094fe931b187dd9f80f8ed4c
Brendon Baughn <Brendon@Marions-MacBook-Pro.local>
2015-09-19 15:23:03 -0400

    working index action
    working index action

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  has_attached_file :image, :styles => { large: '600x600>', medium: '300x300>', thumb: '150x150#' }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
